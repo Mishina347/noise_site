b572e37227e504d9d77fb7df83b56a5c3212b9eb
Mishina347 <mishima0013@gmail.com>
2025-08-06 16:29:04 +0900

    スタイル修正

--- src/interface/components/NoiseController.tsx
+++ src/interface/components/NoiseController.tsx
@@ -7,12 +7,29 @@ export const NoiseController: React.FC = () => {
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
       <h1>🌫️ noise</h1>
-      <button onClick={toggle}>
+      <button 
+        style={{
+          
+          cursor: 'pointer',
+          border:"none",
+          width:"80px",
+          height:"80px",
+          padding:0,
+          borderRadius:"50%",
+          textAlign:'center',
+        }}
+        onClick={toggle} 
+        >
         {isPlaying ? 'pause' : 'play'}
       </button>
-      <div style={{ marginTop: '1rem' }}>
+      <div style={{ marginTop: '2rem' }}>
         <label htmlFor="volume">Volume: </label>
         <input
+          style={{
+            width: '200px',
+            accentColor: '#888', 
+            cursor: 'pointer',
+          }}
           id="volume"
           type="range"
           min="0"
