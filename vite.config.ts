e33c6980749f6ca8b6217a5f1ff4ea7c05fc2f29
Mishina347 <mishima0013@gmail.com>
2025-08-08 02:34:33 +0900

    ビルド時軽量化対応

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,41 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base:"/noise_site/",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      manifest: {
-        name: 'noise',
-        short_name: 'noise',
-        start_url: '/noise_site/',
-        scope: '/noise_site/', 
-        display: 'standalone',
-        background_color: '#111111',
-        theme_color: '#111111',
-        icons: [
-          {
-            src: 'icons/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-});
+	base: '/noise_site/',
+	build: {
+		outDir: 'dist',
+		target: 'esnext',
+		sourcemap: false,
+	},
+	plugins: [
+		react(),
+
+		VitePWA({
+			registerType: 'autoUpdate',
+			injectRegister: 'auto',
+			manifest: {
+				name: 'noise',
+				short_name: 'noise',
+				start_url: '/noise_site/',
+				scope: '/noise_site/',
+				display: 'standalone',
+				background_color: '#111111',
+				theme_color: '#111111',
+				icons: [
+					{
+						src: 'icons/icon-192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'icons/icon-512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
+})
